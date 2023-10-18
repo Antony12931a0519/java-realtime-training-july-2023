495f789fba8ef09eb5f01d9cdb2649352501ff88
Antony Sampath kumar Reddy Avula <antonysampath333@gmail.com>
2023-10-18 23:17:49 +0530

    initial setup

---
+++ collegemanagementsystem/src/main/java/com/realtime/java/training/collegemanagementsystem/controller/WelcomeController.java
@@ -0,0 +1,22 @@
+package com.realtime.java.training.collegemanagementsystem.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	@GetMapping("welcome")
+	public String showWelcomeMessage() {
+		return "Welcome to College Management System :)";
+		
+	}
+	
+
+	@GetMapping("studentslist")
+	public String getstudentsList() {
+		return " :) :)";
+		
+	}
+
+}
